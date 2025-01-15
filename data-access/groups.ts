31779c0868b73f6f7527f4319141c44c29778989
Fredrik <fredrikadr@gmail.com>
2025-01-15 12:58:53 +0100

    move data-access folder

--- data-access/groups.ts
+++ data-access/groups.ts
@@ -18,10 +18,10 @@ export async function createGroup(formData: GroupInterface) {
             .insert([
                 {
                     created_by: formData.created_by,
-                    name: formData.name, 
+                    name: formData.name,
                     description: formData.description,
                     is_public: formData.is_public,
-                    auto_approve: formData.auto_approve, 
+                    auto_approve: formData.auto_approve,
                     show_pending: formData.show_pending,
                 },
             ])
@@ -37,3 +37,23 @@ export async function createGroup(formData: GroupInterface) {
 
 
 }
+
+
+export async function getGroup(groupid: string) {
+    const supabase = await createClient();
+    try {
+        const { data: groups, error } = await supabase
+            .from('groups')
+            .select('*')
+            .eq("id", groupid)
+
+        if (error) {
+            throw error;
+        }
+
+        return groups;
+    } catch (error: any) {
+        console.error("Error fetching group.", error.message)
+    }
+}
+
